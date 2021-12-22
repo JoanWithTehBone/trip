53a3f1e63f45190884fea02049eceb95a5ec5d16
Alexandra Kranendonk <alex@MBPvanAlexandra.home>
2021-12-22 10:49:52 +0100

    Merge changes

--- build/Player.js
+++ build/Player.js
@@ -53,10 +53,10 @@ export default class Player extends GameItem {
         return this.keyboard.isKeyDown(KeyListener.KEY_SPACE);
     }
     collidesWith(other) {
-        return this.xPos < other.getXPos() + other.getImageWidth() + 10
-            && this.xPos + this.img.width + 10 > other.getXPos()
-            && this.yPos < other.getYPos() + other.getImageHeight() + 10
-            && this.yPos + this.img.height + 10 > other.getYPos();
+        return this.xPos < other.getXPos() + other.getImageWidth()
+            && this.xPos + this.img.width > other.getXPos()
+            && this.yPos < other.getYPos() + other.getImageHeight()
+            && this.yPos + this.img.height > other.getYPos();
     }
     interactWithBaker() {
         if (this.collidesWith(this.baker)) {
