474355a585f6781736402fc554cb4b98a93bbfb5
JoanWithTehBone <70522505+JoanWithTehBone@users.noreply.github.com>
2022-01-13 12:12:57 +0100

    Balancing for the Demo

--- src/Game.ts
+++ src/Game.ts
@@ -44,7 +44,7 @@ export default class Game {
    */
   public reset(): void {
     // Initialising the player's stats
-    this.playerStats = new UserData(20, 4, 2);
+    this.playerStats = new UserData(20, 3, 2);
   }
 
   /**
